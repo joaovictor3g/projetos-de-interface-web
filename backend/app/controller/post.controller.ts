e4666367156dc01ba33a213167591a52efede9f4
joaovictor3g <jvdias79797@gmail.com>
2022-08-24 16:05:49 -0300

    feat: creates all activity endpoints

--- backend/app/controller/post.controller.ts
+++ backend/app/controller/post.controller.ts
@@ -5,4 +5,37 @@ export const postController = {
   async index(req: Request, res: Response) {
     return res.json(posts);
   },
+
+  async create(req: Request, res: Response) {
+    const { texto, likes, id } = req.body;
+
+    posts.push({ texto, likes, id });
+
+    return res.status(201).json({ message: "Post criado com sucesso!" });
+  },
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const foundedPost = posts.find((post) => post.id === Number(id));
+
+    if (!foundedPost)
+      return res.status(404).json({ message: "Post não encontrado" });
+
+    return res.json(foundedPost);
+  },
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const postsWithDeletion = posts.filter((post) => post.id !== Number(id));
+
+    if (postsWithDeletion.length === posts.length)
+      return res.status(404).json({ message: "Post não encontrado" });
+
+    return res.json({
+      message: "Post deletado com sucesso",
+      data: postsWithDeletion,
+    });
+  },
 };
