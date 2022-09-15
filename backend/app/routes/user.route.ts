f001cd8d870ac5e972d9d20bb1b1b48ec55c0601
joaovictor3g <jvdias79797@gmail.com>
2022-09-14 21:52:07 -0300

    fix: lets create without token

--- backend/app/routes/user.route.ts
+++ backend/app/routes/user.route.ts
@@ -4,10 +4,10 @@ import { userController } from "../controller/user.controller";
 
 const routes = Router();
 
+routes.post("/user", userController.create);
 routes.use("/user", authController.check);
 routes.get("/user", userController.index);
 routes.get("/user/:id", userController.show);
-routes.post("/user", userController.create);
 routes.delete("/user/:id", userController.delete);
 
 export { routes as userRoutes };
