5a479e701bfb182003482bda2c270cb5ea37cbde
joaovictor3g <jvdias79797@gmail.com>
2022-09-14 21:46:24 -0300

    feat: implements auth

--- backend/app/controller/auth.controller.ts
+++ backend/app/controller/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
-import { User, userModel } from "../model/user.model";
+import { userModel } from "../model/user.model";
+import jwt from "jsonwebtoken";
 
 export const authController = {
   async index(req: Request, res: Response) {
@@ -9,18 +10,30 @@ export const authController = {
     try {
       const user = await userModel.findOne({ email });
 
-      console.log(user);
-
       if (!user)
         return res.status(404).json({ message: "Usuário inexistente" });
 
       if (!bcrypt.compareSync(password, user.password)) {
         return res.status(401).json({ message: "Invalid login" });
       }
-
-      return res.status(200).json({ message: "ok" });
+      const token = jwt.sign({ user }, process.env.JWT_SECRET);
+      return res.status(200).json({ message: "ok", token });
     } catch {
       return res.status(401).json({ message: "Invalid login" });
     }
+  },
+
+  async check(req: Request, res: Response, next: NextFunction) {
+    const { token } = req.headers;
+
+    jwt.verify(token as string, process.env.JWT_SECRET, (error) => {
+      if (error) {
+        return res
+          .status(401)
+          .json({ message: "Usuário não autenticado", error: error.message });
+      }
+
+      next();
+    });
   },
 };
